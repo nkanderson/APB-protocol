15a435cd7c617ace52496d1b07eea0bc2d0f1166
Niklas Anderson <nkanders@gmail.com>
2025-02-22 15:37:54 -0800

    Try using full file path Troubleshooting issue with script not finding source files

--- sim/run_questa.tcl
+++ sim/run_questa.tcl
@@ -1,15 +1,18 @@
 # Set up the simulation environment
+set SRC_DIR [file normalize "$env(PWD)/src"]
+set TB_DIR [file normalize "$env(PWD)/tb"]
+
 quit -sim
 vlib work
 vmap work work
 
 # Compile source files
-vlog -sv ../src/apb_pkg.sv
-vlog -sv ../src/apb_interface.sv
-vlog -sv ../src/apb_peripheral.sv
+vlog -sv "$SRC_DIR/apb_pkg.sv"
+vlog -sv "$SRC_DIR/apb_interface.sv"
+vlog -sv "$SRC_DIR/apb_peripheral.sv"
 
 # Compile testbench
-vlog -sv ../tb/apb_tb.sv
+vlog -sv "$TB_DIR/apb_tb.sv"
 
 # Run simulation
 vsim -voptargs=+acc work.apb_tb
