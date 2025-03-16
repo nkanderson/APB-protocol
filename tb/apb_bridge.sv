180a0cb39818dd9df13fcf8e68f1efa0a74ba08d
Nothari117 <hari36krishnan@gmail.com>
2025-03-16 12:21:38 -0700

    added tasks for write
    Co-Authored-By: nkanderson <4128825+nkanderson@users.noreply.github.com>
    Co-Authored-By: GowthamiMedarametla <187230978+GowthamiMedarametla@users.noreply.github.com>
    Co-Authored-By: tnl3pdx <98623531+tnl3pdx@users.noreply.github.com>

--- tb/apb_bridge.sv
+++ tb/apb_bridge.sv
@@ -141,10 +141,83 @@ module apb_bridge (
 
     $display("Invalid Read Test Completed.");
   endtask
-  
-  task test_write();
+    
+  // Task for performing APB Write
+  task test_write(input logic [7:0]addr,input logic [31:0] data);
+    @(posedge PCLK);
+    psel =1;
+    pwrite =1;
+    paddr = addr;
+    pwdata = data;
+    penable =0;
 
+    @(posedge pclk);
+    penable=1;
 
+    wait(pready);
+
+    @(posedge pclk);
+    psel = 0;
+    penable =0;
+    $display("APB Write: Address =%h, Data =%h, addr,data");
+  endtask
+
+  // Task for APB Read
+  task test_read(input logic [7:0] addr);
+   @(posedge pclk);
+   psel= 1;
+   pwrite =0;
+   paddr = addr; 
+   penable =0;
+
+   @(posedge pclk);
+   penable =1;
+
+   wait(pready);
+
+   @(posedge pclk);
+   psel =0;
+   penable=0;
+   $display("APB Read: Address =%h, Data =%h addr, prdata");
+  endtask
+
+  //Task for invalid write operation
+  task test_invalid_write();
+   @(posedge pclk);
+   psel =1;
+   pwrite =1;
+   paddr =8'hFF; //invalid address
+   pwdata = 32'hDEADBEEF;
+   penable=0;
+
+   @(posedge pclk);
+   penable =1;
+
+   wait(pready);
+
+   @(posedge pclk);
+   psel =0;
+   penable=0;
+   assert (pslverr) else $error("Invalid Write Test Failed");
+  endtask
+
+  // Test for invalid read operation
+  task test_invalid_read();
+    @(posedge pclk);
+    psel = 1;
+    pwrite= 0;
+    paddr = 8'hFF; // invalid address
+    penable= 0;
+
+    @(posedge pclk);
+    penable =1;
+
+    wait(pready);
+
+    @(posedge pclk);
+    psel=0;
+    penable=0;
+    assert (pslverr) else $error("Invalid Read Test Failed");
   endtask
 
   task reset_apb();
