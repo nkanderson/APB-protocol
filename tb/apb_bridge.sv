7b6785518adb9b98d567599ce396812aabbd88d5
Niklas Anderson <nkanders@gmail.com>
2025-03-17 14:55:55 -0700

    Add test for sparse data write using strobe Currently fails since z values are written to the strobe-low bytes. But the strobe-high byte in the test is correctly written to.

--- tb/apb_bridge.sv
+++ tb/apb_bridge.sv
@@ -18,6 +18,7 @@ module apb_bridge (
 
   logic [apb.ADDR_WIDTH-1:0] test_addr = {{(apb.ADDR_WIDTH - 4) {1'b0}}, 4'h4};
   logic [apb.DATA_WIDTH-1:0] read_data;
+  logic [apb.DATA_WIDTH-1:0] read_data_initial; // Used to test sparse data writes
   logic [apb.DATA_WIDTH-1:0] write_data;
   logic [2:0] pprot;
   logic [2:0] pprot_bits_invert;
@@ -81,6 +82,15 @@ module apb_bridge (
     test_write(.addr(test_addr), .pprot(pprot), .should_err(0), .reset(1), .data(write_data));
     test_read(.addr(test_addr), .pprot(pprot), .should_err(0), .reset(1), .data(read_data));
     assert (read_data == '1) else $error("Written data not correctly read back.");
+
+    // Test a sparse data write using the strobe signal
+    // We'll write to a different address to test, leave write_data the same (all 1s),
+    // but update the strobe to only write the lowest byte
+    test_addr = test_addr | 8'h80;
+    test_read(.addr(test_addr), .pprot(pprot), .data(read_data_initial));
+    test_write(.addr(test_addr), .pprot(pprot), .data(write_data), .strobe(4'h1));
+    test_read(.addr(test_addr), .pprot(pprot), .data(read_data));
+    assert (read_data == (read_data_initial | 4'h1)) else $error("Sparse data write not correctly read back.");
 
     // Wait a few cycles before finishing
     repeat (4) @(posedge apb.pclk);
